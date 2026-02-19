93f315581603192c8c7699b5c34c8f195f8f5fa7
Hemant Tengale <tengalehemant2@gmail.com>
2026-02-19 16:55:15 +0530

    Modify jenkins.sh for OpenJDK 21 and keyring
    Updated Jenkins installation script to use OpenJDK 21 and added keyring setup.

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
 sudo apt update -y
+### Java
 #sudo apt install openjdk-17-jdk -y
 sudo apt install fontconfig openjdk-21-jre
 sudo apt update -y
+### Maven
 sudo apt install maven -y
+### Jenkins
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
