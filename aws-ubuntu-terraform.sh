790bb322399838fc4fd44edc84810cdebd66d5bb
Hemant Tengale <tengalehemant2@gmail.com>
2026-02-21 20:27:14 +0530

    Add script to install Terraform on Ubuntu
    This script installs HashiCorp's GPG key and adds the official repository for Terraform on Ubuntu.

---
+++ aws-ubuntu-terraform.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# HashiCorp's GPG signature and install HashiCorp's Debian package repository.
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+# Install HashiCorp's GPG key.
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Verify the GPG key's fingerprint.
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+
+# Add the official HashiCorp repository to your system.
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update apt to download the package information from the HashiCorp repository.
+sudo apt update
+
+# Install Terraform from the new repository.
+sudo apt-get install terraform
