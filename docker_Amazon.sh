94f71a087ad19bbceafcc6e401781136d423796b
Hemant Tengale <tengalehemant2@gmail.com>
2026-02-22 17:55:00 +0530

    Add script to install and configure Docker on EC2

---
+++ docker_Amazon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Update the installed packages and package cache on your instance.
+sudo yum update -y
+# Install the most recent Docker Community Edition package.
+sudo yum install -y docker
+# Start the Docker service.
+sudo service docker start
+# Enable Docker to start on boot
+sudo service docker enable
+# Check Docker service status
+sudo systemctl status docker
+# Add the ec2-user to the docker group so that you can run Docker commands without using sudo.
+sudo usermod -a -G docker ec2-user
+newgrp docker
+# Installing Docker Compose
+sudo dnf install -y docker-compose-plugin
